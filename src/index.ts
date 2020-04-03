abfd3faa7bd5d97c67773e12acb778f74ba8577e
taoqf <tao_qiufeng@126.com>
2020-04-03 16:11:46 +0800

    init widget ww000004

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import on, { emit } from '@mmstudio/on';
 
-const no = 'mm-001';
+const no = 'mm-000004';
 
 /**
  * :host表示选中当前组件,使用all:initial;属性,
  * 当外部有css样式要越过shadowDom边界时将该css样式重置为初始值,从而不在影响shaowDom内部的样式.
  * 参考链接https://developers.google.cn/web/fundamentals/web-components/shadowdom#reset,
- * 使用 ${no} 作为所有样式类的前缀可以作为前置限定，从而避免某些浏览器对shadowdom支持不好，样式错乱的问题,
+ * 使用 000004 作为所有样式类的前缀可以作为前置限定，从而避免某些浏览器对shadowdom支持不好，样式错乱的问题,
  * tpl 中可以添加link标签，但是最好使用公网可以访问到的cdn连接，且一定要保证版本号固定，如<link rel="stylesheet" href="//cdn.jsdelivr.net/npm/jstree@3.3.7/dist/themes/default/style.min.css" />
  */
 const tpl = `
@@ -23,7 +23,7 @@ const tpl = `
 </style>
 
 <div class="${no}">
-	<div id="n01">Hello <a href="https://mm-edu.gitee.io/">mmstudio</a></div>
+	<div id="n01">Hello <a href="https://mmstudio.gitee.io/">mmstudio</a></div>
 </div>
 `;
 
@@ -42,14 +42,14 @@ export default class Widget extends HTMLElement {
 				id: this.id
 			}
 		});
-		alert(`widget ${no} inited, attribte: sattr = ${s}`);
+		alert(`widget ${no} inited, attribte: sattr = ${s} `);
 	}
 	/**
 	 * 方法注释
 	 * @param param 参数注释
 	 */
 	public method01(param: string) {
-		window.console.debug('this dddis a public method', param);
+		window.console.debug('this is a public method', param);
 		this.method02(param);
 	}
 	private method02(param: string) {
@@ -60,3 +60,4 @@ export default class Widget extends HTMLElement {
 if (!window.customElements.get(no)) {
 	window.customElements.define(no, Widget);
 }
+
